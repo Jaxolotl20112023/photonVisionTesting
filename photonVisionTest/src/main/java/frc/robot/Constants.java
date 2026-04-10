12385b34353f7cb64e9b10d66534d38fb0b04f06
Shimazu <jaxolotl7@gmail.com>
2026-04-09 16:00:35 -1000

    added the photon code and the swerve code

--- photonVisionTest/src/main/java/frc/robot/Constants.java
+++ photonVisionTest/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +21,20 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class PhotonVisionConst {
+
+    public static final AprilTagFieldLayout field_layout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark); 
+
+    public static final Transform3d blackCameraOffset = new Transform3d(
+      new Translation3d(0,0,0),
+      new Rotation3d(0,0,0)
+    );
+
+    public static final Transform3d blueCameraOffset = new Transform3d(
+      new Translation3d(0,0,0),
+      new Rotation3d(0,0,0)
+    );
   }
 }
--- photonVisionTest/src/main/java/frc/robot/subsystems/PhotonVision.java
+++ photonVisionTest/src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -1,14 +1,111 @@
 package frc.robot.subsystems;
 
-public class PhotonVision {
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class PhotonVision extends SubsystemBase {
+    private final CommandSwerveDrivetrain drivetrain; 
+    private final PhotonCamera camera; 
+    private final PhotonPoseEstimator poseEstimator; 
+
+    private List<PhotonPipelineResult> results; 
+    private Transform3d tagToCamera; 
+    private Transform2d tagToCameraProcessed;
+    private Pose2d estimatedRobotPoseRelative; 
+    private EstimatedRobotPose estimatedRobotPoseField; 
+    private Transform3d cameraToRobot; 
+
+    private Optional<PhotonPipelineResult> currentResult;
+
+    private int tagID; 
+    private String cameraName; 
     
 
-    public PhotonVision() {
+    public PhotonVision(String cameraName, CommandSwerveDrivetrain drivetrain, Transform3d cameraToRobot) {
+        this.drivetrain = drivetrain;
+        this.cameraName = cameraName; 
+
+        this.camera = new PhotonCamera(cameraName); 
+        this.cameraToRobot = cameraToRobot;
+        poseEstimator = new PhotonPoseEstimator(Constants.PhotonVisionConst.field_layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraToRobot);  
 
     }
 
     @Override
     public void periodic() {
-    
+        results = camera.getAllUnreadResults(); 
+        currentResult = Optional.empty();
+
+        // look at all the tags seen
+        for (var change : results)  {
+            currentResult = Optional.of(change); 
+        }
+
+        // if there is no tags seen then get out of function
+        if (currentResult.isEmpty()) {
+            return; 
+        }
+
+        // get the best tag seen 
+        PhotonTrackedTarget target = currentResult.get().getBestTarget(); 
+
+        tagID = target.getFiducialId(); 
+
+        // calc the 2d pose of the robot using the field loaded and the of the tag
+        Pose2d tagFieldRelativePose = (Constants.PhotonVisionConst.field_layout).getTagPose(tagID).get().toPose2d();
+        
+        SmartDashboard.putNumber("Current Seen Tag Field X", tagFieldRelativePose.getX());
+        SmartDashboard.putNumber("Current Seen Tag Field Y", tagFieldRelativePose.getY());
+        SmartDashboard.putNumber("Current Seen Tag Field Yaw", tagFieldRelativePose.getRotation().getRadians());
+
+        tagToCameraProcessed = PhotonUtils.estimateCameraToTarget(tagToCamera.getTranslation().toTranslation2d(), 
+            tagFieldRelativePose, 
+            drivetrain.getRotation3d().toRotation2d());
+        
+        // calc the field relative position of the robot by using the tag to camera and the tag field, as well as the rotation of the camera to robot
+        estimatedRobotPoseRelative = PhotonUtils.estimateFieldToRobot(tagToCameraProcessed, 
+            tagFieldRelativePose, 
+            new Transform2d(cameraToRobot.getTranslation().toTranslation2d(), cameraToRobot.getRotation().toRotation2d()));
+
+        if(poseEstimator.estimateCoprocMultiTagPose(currentResult.get()).isPresent()) {
+            estimatedRobotPoseField = poseEstimator.estimateCoprocMultiTagPose(currentResult.get()).get();
+            drivetrain.addVisionMeasurement(estimatedRobotPoseRelative, Timer.getFPGATimestamp()); 
+            setSmartDashboard();
+        }
     }
+
+    private void setSmartDashboard() {
+        
+        SmartDashboard.putNumber(cameraName+" Camera ID", tagID);
+        SmartDashboard.putBoolean(cameraName+" Tags Detected", currentResult.isPresent());
+
+        SmartDashboard.putNumber(cameraName+" camera to target yaw", tagToCamera.getRotation().toRotation2d().getDegrees()); 
+        SmartDashboard.putNumber(cameraName+" camera to target x", tagToCamera.getX()); 
+        SmartDashboard.putNumber(cameraName+" camera to target y", tagToCamera.getY()); 
+
+        SmartDashboard.putNumber(cameraName+" pigeon readings", drivetrain.getPigeon2().getYaw().getValueAsDouble()); 
+        SmartDashboard.putNumber(cameraName+" odometry readings yaw", drivetrain.getState().Pose.getRotation().getDegrees());
+        SmartDashboard.putNumber(cameraName+" odometry reading x", drivetrain.getState().Pose.getX()); 
+        SmartDashboard.putNumber(cameraName+" odometry reading y", drivetrain.getState().Pose.getY());  
+
+    }
+
+
 }
